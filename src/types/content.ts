42cd8667b651f049e386898b4e1a3d53ac5c8c50
Ben <benharris@manx.net>
2026-04-02 23:11:11 +0100

    some initial scaffolding

---
+++ src/types/content.ts
@@ -0,0 +1,18 @@
+export type Author = {
+
+};
+
+export type Article = {
+
+};
+
+export type Edition = {
+    
+}
+
+export type NavigationSection = {
+    section: string;
+    subsections:string[];
+    // add seo for page for each main section.... optional?
+    // add display name for navbar 
+}
