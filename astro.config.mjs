49382bf2fe3dc134f53235fec0f69be58885d651
Ben <benharris@manx.net>
2026-04-04 13:25:04 +0100

    added deploy to cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,16 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
 import sitemap from '@astrojs/sitemap';
 
+import cloudflare from '@astrojs/cloudflare';
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [sitemap()],
+
   vite: {
     plugins: [tailwindcss()],
   },
+
   site: 'https://nouse.co.uk',
+  adapter: cloudflare(),
 });
