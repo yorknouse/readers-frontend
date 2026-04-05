56f841a7279d0125f53b21d458978567ad4d9b8d
Ben <benharris@manx.net>
2026-04-05 23:03:02 +0100

    more foundations

--- src/types/content.ts
+++ src/types/content.ts
@@ -1,12 +1,23 @@
+export type Publication = 'nouse' | 'muse';
+export type PageType = 'section' | 'article' | 'info';
+
+export type Page = {
+  name: string;
+  slug: string;
+  description: string;
+  title: string;
+  publication: Publication;
+  type: PageType;
+  colour?: string;
+  hover_colour?: string;
+  text_colour?: string;
+  border_colour?: string;
+  og_image?: string;
+  parent_slug?: string;
+};
+
 export type Author = {};
 
 export type Article = {};
 
 export type Edition = {};
-
-export type NavigationSection = {
-  section: string;
-  subsections: string[];
-  // add seo for page for each main section.... optional?
-  // add display name for navbar
-};
