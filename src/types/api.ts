4be74ee8cb0e7f201424dbca47ea45c5c8781599
Ben <benharris@manx.net>
2026-04-16 13:34:21 +0100

    updating types

---
+++ src/types/api.ts
@@ -0,0 +1,13 @@
+export type User = {
+  users_userid: number;
+  users_name1: string;
+  users_name2: string;
+  users_googleAppsUsernameNouse: string;
+}
+
+export type Position = {
+  positions_id: number;
+  positions_displayName: string;
+  positions_teamPageGroup: number;
+  users: User[];
+}
